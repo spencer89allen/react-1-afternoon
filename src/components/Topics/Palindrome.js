63eeaffda561f00d8b3a70c9180ec266ecd6cad9
Spencer Allen <spencerallen@Spencers-MacBook-Pro.local>
2019-01-24 20:37:10 -0700

    steps 1 through 6 completed

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -4,14 +4,51 @@ import React, { Component } from 'react';
 
 class Palindrome extends Component {
 
+    constructor() {
+        super()
 
+        this.state = {
+            userInput: '',
+            palindrome: '',
+        }
+
+        this.handleUserInput = this.handleUserInput.bind(this)
+
+    }
+
+
+    handleUserInput(e) {
+        this.setState({
+            userInput: e
+        })
+    }
+
+    handlePalindrome(userInput) {
+        let newStr = userInput.toLowerCase().split('').reverse().join('')
+        if( newStr === userInput.toLowerCase() ) {
+            this.setState({
+                palindrome: true
+            })
+        }
+        else {
+            this.setState({
+                palindrome: false
+            })
+        }
+    }
 
     render() {
         return (
-            <div>
-                <p>
-                    Palindrome.
-                </p>
+            <div className='puzzleBox filterStringPB'>
+
+                <h4>Palindrome</h4>
+
+                <input className='inputLine' onChange={(e) => this.handleUserInput(e.target.value)}></input>
+
+                <button className='confirmationButton' onClick={() => this.handlePalindrome(this.state.userInput)}>Confirm</button>
+
+                <span className='resultsBox'>Results: {JSON.stringify(this.state.palindrome)}</span>
+
             </div>
         )
     }
